e05af7e46ac0255a1182a0482b07845a3d428f79
Cameron Lynch <clynch06@gmail.com>
2014-12-27 10:31:47 -0500

    input validation added

--- assignment3.R
+++ assignment3.R
@@ -1,13 +1,47 @@
 ## Programming Assignment 3 - Hospital Data
 
-outcome <- read.csv("outcome-of-care-measures.csv", 
-                    colClasses = "character", 
-                    na.strings = "Not Available")
-head(outcome)
+best <- function(state, outcome) {
+  ## Read outcome data
+  
+  ## Check that state and outcome are valid
+  valid.states <- outcomes[,7]
+  valid.states <- unique(valid.states)
+  valid.outcomes <- c("heart attack", "heart failure", "pneumonia")
+  valid <- 0
+  for (st in valid.states) {
+    if(st == state)
+    {
+      valid <- 1
+    }
+  }
+  if (valid == 0)
+  {
+    stop("invalid state")
+  }
+  valid <- 0
+  for (illness in valid.outcomes)
+  {
+    if(illness == outcome)
+    {
+      valid <- 1
+    }
+  }
+  if(valid == 0)
+  {
+    stop("invalid outcome")
+  }
+  
+  ## Return hospital name in the state with lowest 30-day death rate
+}
+
+outcomes <- read.csv("outcome-of-care-measures.csv", 
+                     colClasses = "character", 
+                     na.strings = "Not Available")
+head(outcomes)
 
 print("How many columns are present?")
-print(ncol(outcome))
+print(ncol(outcomes))
 
 ## Create a 30-day histogram death rates from heart attack (col 11)
-outcome[,11] <- as.numeric(outcome[,11])
-hist(outcome[,11])
+outcomes[,11] <- as.numeric(outcomes[,11])
+hist(outcomes[,11])
