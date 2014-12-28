a353998a78888d2e3102bda36a73c8276dbc1b7f
Cameron Lynch <clynch06@gmail.com>
2014-12-28 14:12:49 -0500

    Initial commit

---
+++ rankall.R
@@ -0,0 +1,13 @@
+## rankall
+
+
+rankall <- function(outcome, num = "best") {
+  ## Read outcome data
+  
+  ## Check that outcome is valid
+  
+  ## For each state, find the hospital of the given rank
+  
+  ## Return a data frame with the hospital names and the
+  ## (abbreviated) state name
+}
